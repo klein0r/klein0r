8f2626a1594d05ee1a7e387534ad38bf39345c0c
Matthias Kleine <github.com9@emailu.de>
2023-11-09 20:23:17 +0100

    Generate readme

--- README.md
+++ README.md
@@ -1,7 +1,43 @@
-# ioBroker Adapter List
+<p align=center>
+  <a href="https://www.youtube.com/c/Hausautomatisierung-com/"><img alt="YouTube Channel Subscribers" src="https://img.shields.io/youtube/channel/subscribers/UCRDCsZvUg75Bibp9qYbHivw?label=haus-automatisierung.com&logo=youtube&style=flat-square"> <img alt="YouTube Channel Views" src="https://img.shields.io/youtube/channel/views/UCRDCsZvUg75Bibp9qYbHivw?logo=youtube&style=flat-square"></a> <img alt="GitHub followers" src="https://img.shields.io/github/followers/klein0r?logo=github&style=flat-square"> <img alt="GitHub User's stars" src="https://img.shields.io/github/stars/klein0r?affiliations=OWNER&logo=github&style=flat-square">
+</p>
 
-Das ist: 
+My name is **Matthias Kleine**.
+
+Self-employed trainer and software developer based in Paderborn, Germany.
+
+💬 Ask me about **Smart Home** and **Building automation**
+
+<p align="center">
+  <img src="https://github-readme-stats.vercel.app/api?username=klein0r&show_icons=true&theme=calm" alt="klein0r" />
+</p>
 
 ----
 
-*Generated at: 2023-11-09T15:55:21.519Z*
+### ioBroker
+
+#### ioBroker adapter list (my adapters)
+
+| Adapter           | Beta-Version     | Stable-Version     | Installations     | Weekly Downloads  |
+|-------------------|------------------|--------------------|-------------------|-------------------|
+| ioBroker.trashschedule | 2.3.0 | 2.3.0 | 11859 | 2885 |
+| ioBroker.birthdays | 2.4.1 | 2.3.1 | 1386 | 92 |
+| ioBroker.luftdaten | 3.1.0 | 3.1.0 | 1775 | 300 |
+| ioBroker.octoprint | 5.1.0 | 5.1.0 | 2941 | 711 |
+| ioBroker.youtube | 4.4.0 | 4.4.0 | 449 | 46 |
+| ioBroker.lametric | 3.1.1 | 3.1.1 | 413 | 115 |
+| ioBroker.gira-iot | 0.4.0 | - | 180 | 7 |
+| ioBroker.awtrix-light | 0.10.0 | 0.10.0 | 1298 | 419 |
+
+#### ioBroker adapter list (heavy contribution)
+
+| Adapter           | Beta-Version     | Stable-Version     | Installations     | Weekly Downloads  |
+|-------------------|------------------|--------------------|-------------------|-------------------|
+| ioBroker.shelly | 6.6.1 | 6.6.1 | 28752 | 5001 |
+| ioBroker.pvforecast | 2.9.0 | 2.9.0 | 5328 | 1483 |
+| ioBroker.statistics | 2.4.0 | 2.3.0 | 3669 | 359 |
+| ioBroker.proxmox | 2.2.2 | 2.2.1 | 4287 | 2136 |
+
+----
+
+*Generated at: 2023-11-09T19:22:50.967Z*
--- index.js
+++ index.js
@@ -14,33 +14,82 @@ const adapters = [
     'gira-iot',
     'awtrix-light',
     'comfoairq',
+];
+
+const adaptersContrib = [
     'shelly',
     'pvforecast',
     'statistics',
-    'proxmox'
-];
+    'proxmox',
+]
 
 function generateReadme(templateData) {
     const MUSTACHE_TEMPLATE = './README.mustache';
 
     try {
         const template = fs.readFileSync(MUSTACHE_TEMPLATE);
-        const output = Mustache.render(template.toString(), data);
+        const output = Mustache.render(template.toString(), templateData);
         fs.writeFileSync('README.md', output);
+
+        console.log('generated README...');
     } catch (err) {
         console.error(`Unable to render mustache file "${MUSTACHE_TEMPLATE}": ${err}`);
     }
 }
 
+async function getData(url) {
+    const response = await axios.get('http://download.iobroker.net/sources-dist-latest.json', { responseType: 'json', timeout: 5000 });
+    if (response.status === 200) {
+        return response.data;
+    }
+
+    return null;
+}
+
 (async () => {
+    console.log('started...');
+
     let templateData = {
-        generatedAt: new Date().toISOString()
+        generatedAt: new Date().toISOString(),
+        adapters: [],
+        adaptersContrib: [],
     };
 
-    const lastRepoResponse = await axios.get('http://download.iobroker.net/sources-dist-latest.json', { responseType: 'json', timeout: 5000 });
-    if (lastRepoResponse.status === 200) {
+    const betaRepos = await getData('http://download.iobroker.net/sources-dist-latest.json');
 
+    for (const adapter of adapters) {
+        if (betaRepos[adapter]) {
+            const adapterData = betaRepos[adapter];
 
-        generateReadme(templateData);
+            templateData.adapters.push({
+                name: adapterData.name,
+                installations: adapterData.stat,
+                weekDownloads: adapterData.weekDownloads,
+                version: {
+                    beta: adapterData.version,
+                    stable: adapterData.stable ?? '-',
+                },
+            });
+        }
     }
+
+    for (const adapter of adaptersContrib) {
+        if (betaRepos[adapter]) {
+            const adapterData = betaRepos[adapter];
+
+            templateData.adaptersContrib.push({
+                name: adapterData.name,
+                installations: adapterData.stat,
+                weekDownloads: adapterData.weekDownloads,
+                version: {
+                    beta: adapterData.version,
+                    stable: adapterData.stable ?? '-',
+                },
+            });
+        }
+    }
+
+    generateReadme(templateData);
+
+    console.log('done...');
 })();
