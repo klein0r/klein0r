ac6060f4d79f5730e55995cf3837d4a2eaa9753e
Matthias Kleine <github.com9@emailu.de>
2025-05-19 11:37:06 +0200

    x-ratelimit-remaining

--- utils/http.js
+++ utils/http.js
@@ -5,6 +5,12 @@ const axios = require('axios').default;
 async function getText(url) {
     console.log(`downloading text: ${url}`);
     const response = await axios.get(url, { responseType: 'text', timeout: 5000 });
+
+    const rateLimitRemaining = response?.headers?.['x-ratelimit-remaining'];
+    if (rateLimitRemaining) {
+        console.log(`  x-ratelimit-remaining: ${rateLimitRemaining}`);
+    }
+
     if (response.status === 200) {
         return response.data;
     }
@@ -15,6 +21,12 @@ async function getText(url) {
 async function getData(url) {
     console.log(`downloading data: ${url}`);
     const response = await axios.get(url, { responseType: 'json', timeout: 5000 });
+
+    const rateLimitRemaining = response?.headers?.['x-ratelimit-remaining'];
+    if (rateLimitRemaining) {
+        console.log(`  x-ratelimit-remaining: ${rateLimitRemaining}`);
+    }
+
     if (response.status === 200) {
         return response.data;
     }
