6aa65c3c95159226fff6abbb57508154bb21767f
Matthias Kleine <github.com9@emailu.de>
2026-07-29 16:24:31 +0200

    Added caching

--- utils/http.js
+++ utils/http.js
@@ -1,13 +1,44 @@
 'use strict';
 
 const axios = require('axios').default;
+const fs = require('node:fs');
+const path = require('node:path');
+const crypto = require('node:crypto');
 
 function sleep (time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function hashForCache(url) {
+    console.log(`hashing url (${typeof url}): ${url}`);
+    return crypto.createHash('md5').update(url).digest('hex');
+}
+
+function getCached(url) {
+    const filePath = path.join(__dirname, '../.cache/', hashForCache(url));
+    return fs.existsSync(filePath) ? fs.readFileSync(filePath, { encoding: 'utf8' }) : null;
+}
+
+function saveCached(url, content) {
+    const cachePath = path.join(__dirname, '../.cache/');
+    if (!fs.existsSync(cachePath)) {
+        fs.mkdirSync(cachePath);
+    }
+
+    if (typeof content === 'string') {
+        const filePath = path.join(cachePath, hashForCache(url));
+        fs.writeFileSync(filePath, content, { encoding: 'utf8' });
+    }
 }
 
 async function getText(url) {
     try {
+        const cachedFile = getCached(url);
+        if (cachedFile !== null) {
+            console.log(`loaded data (${typeof cachedFile}) from cache for: ${url}`);
+            return cachedFile;
+        }
+
         console.log(`downloading text: ${url}`);
         const response = await axios.get(url, { responseType: 'text', timeout: 5000 });
 
@@ -17,6 +48,7 @@ async function getText(url) {
         }
 
         if (response.status === 200) {
+            saveCached(url, response.data);
             return response.data;
         }
 
@@ -28,6 +60,12 @@ async function getText(url) {
 }
 
 async function getData(url) {
+    const cachedFile = getCached(url);
+    if (cachedFile !== null) {
+        console.log(`loaded data (${typeof cachedFile}) from cache for: ${url}`);
+        return JSON.parse(cachedFile);
+    }
+
     console.log(`downloading data: ${url}`);
     const response = await axios.get(url, { responseType: 'json', timeout: 5000 });
 
@@ -41,6 +79,7 @@ async function getData(url) {
     }
 
     if (response.status === 200) {
+        saveCached(url, JSON.stringify(response.data, null, 2));
         return response.data;
     }
 
